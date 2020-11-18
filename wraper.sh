b20ae7d1311aafc7d201eb911d61b68d513bf2ca
karthik <kreddy8512@gmail.com>
2020-11-17 19:55:26 -0800

    updating

--- wraper.sh
+++ wraper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 zip -r sample-app.zip . -x '*.git*'
 #tar -zcv --exclude='.git' --exclude='.gitignore' -f sample-app.tgz *
-aws s3 cp sample-app.zip s3://pythonmysqlapp/
+aws s3 cp sample-app.zip s3://pythonmysqlapp/pythonapp/
